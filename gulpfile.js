f5e2e02a78fa5e04843322325a83c9e454bc7a9e
Mátyás Mustoha <matyas.mustoha@gmail.com>
2019-02-13 17:56:44 +0100

    Gulp update

--- gulpfile.js
+++ gulpfile.js
@@ -1,96 +1,58 @@
 'use strict';
 
-// Creates an optimized release from the mkdocs build
+const gulp = require('gulp'),
+    cleancss = require('gulp-clean-css'),
+    del = require('del'),
+    htmlmin = require('gulp-htmlmin'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify-es').default;
 
 
-let del = require('del');
-let gulp = require('gulp');
-let gulpLoadPlugins = require('gulp-load-plugins');
-var pump = require('pump');
-let runSequence = require('run-sequence');
-
-const $ = gulpLoadPlugins();
-
-
-let DEST_PATH = 'build-dist';
-
+const DEST_PATH = 'build';
 const htmlminConfig = {
-  collapseBooleanAttributes: true,
-  collapseWhitespace: true,
-  removeAttributeQuotes: true,
-  removeComments: true,
-  removeEmptyAttributes: true,
-  removeOptionalTags: false,
-  removeRedundantAttributes: true,
-  removeScriptTypeAttributes: true,
-  removeStyleLinkTypeAttributes: true,
+    collapseBooleanAttributes: true,
+    collapseWhitespace: true,
+    removeAttributeQuotes: true,
+    removeComments: true,
+    removeEmptyAttributes: true,
+    removeOptionalTags: false,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
 };
 
 
-gulp.task('html', callback => {
-  pump([
-      gulp.src('site/**/*.html'),
-      $.htmlmin(htmlminConfig),
-      $.size({title: 'html', showFiles: true}),
-      gulp.dest(DEST_PATH),
-    ],
-    callback
-  );
-});
+gulp.task('html', () => gulp
+    .src('site/**/*.html')
+    .pipe(htmlmin(htmlminConfig))
+    .pipe(gulp.dest(DEST_PATH))
+);
 
-gulp.task('css', callback => {
-  pump([
-      gulp.src('site/**/*.css'),
-      $.cleanCss(),
-      $.size({title: 'css'}),
-      gulp.dest(DEST_PATH),
-    ],
-    callback
-  );
-});
+gulp.task('css', () => gulp
+    .src('site/**/*.css')
+    .pipe(cleancss())
+    .pipe(gulp.dest(DEST_PATH))
+);
 
-gulp.task('js', callback => {
-  pump([
-      gulp.src('site/**/*.js'),
-      $.uglifyEs.default(),
-      $.size({title: 'js'}),
-      gulp.dest(DEST_PATH)
-    ],
-    callback
-  );
-});
+gulp.task('js', () => gulp
+    .src('site/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest(DEST_PATH))
+);
 
-gulp.task('images', callback => {
-  pump([
-      gulp.src('site/**/*.{jpg,png}'),
-      $.imagemin({verbose: true}),
-      $.size({title: 'images'}),
-      gulp.dest(DEST_PATH),
-    ],
-    callback
-  );
-});
+gulp.task('img', () => gulp
+    .src('site/**/*.{jpg,png}')
+    .pipe(imagemin({ verbose: true }))
+    .pipe(gulp.dest(DEST_PATH))
+);
 
-gulp.task('data', callback => {
-  pump([
-      gulp.src('site/**/*.{json,xml,eot,svg,ttf,woff,mustache,ico,webm}'),
-      $.size({title: 'data'}),
-      gulp.dest(DEST_PATH),
-    ],
-    callback
-  );
-});
+gulp.task('data', () => gulp
+    .src('site/**/*.{json,xml,eot,svg,ttf,woff,mustache,ico,webm}')
+    .pipe(gulp.dest(DEST_PATH))
+);
+
 
 gulp.task('clean', () => del([DEST_PATH + '/*']));
 
-
-function do_build(callback) {
-  runSequence(
-    'clean',
-    ['html', 'css', 'js', 'images', 'data'],
-    callback
-  );
-}
-
-gulp.task('default', callback => do_build(callback));
-gulp.task('build', callback => do_build(callback));
+gulp.task('default', ['html', 'css', 'js', 'img', 'data']);
+gulp.task('build', ['default']);
